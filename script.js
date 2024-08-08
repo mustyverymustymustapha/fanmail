0f6155d1be3f3ea218980d07604ad8d6ba05f0ff
mustyverymustymustapha <mustaphasatti1@gmail.com>
2024-08-08 14:03:24 +0000

    hehehe

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ function submitName() {
     }
 }
 
+function generateFanMail() {
+    const greetings = ["Dear", "Hello", "Hi", "Hey"];
+    const adjectives = ["amazing", "incredible", "awesome", "fantastic", "wonderful"];
+    const nouns = ["fan", "supporter", "follower", "admirer"];
+    const compliments = [
+        "Your work is truly inspiring!",
+        "I'm in awe of your talent!",
+        "You're making a real difference!",
+        "Your creativity knows no bounds!",
+        "You're a true role model!"
+    ];
+    const closings = ["Best wishes", "Yours truly", "Sincerely", "With admiration"];
+
+    const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const noun = nouns[Math.floor(Math.random() * nouns.length)];
+    const compliment = compliments[Math.floor(Math.random() * compliments.length)];
+    const closing = closings[Math.floor(Math.random() * closings.length)];
+
+    const subject = `Fan Mail #${++emailCount}`;
+    const content = `${greeting} ${adjective} ${noun},<br><br>
+                     ${compliment}<br><br>
+                     ${closing},<br>
+                     Your biggest fan`;
+
+    addEmailToList(subject, content);
+}
+
+function addEmailToList(subject, content) {
+    const emailList = document.getElementById('emailList');
+    const emailItem = document.createElement('div');
+    emailItem.className = 'email-item';
+    emailItem.innerHTML = `
+        <div class="email-subject">${subject}</div>
+        <div class="email-content">${content}</div>
+    `;
+    emailItem.addEventListener('click', function() {
+        this.classList.toggle('open');
+    });
+    emailList.prepend(emailItem);
+}
